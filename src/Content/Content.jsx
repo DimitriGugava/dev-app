9d75dcaa2493830b128c6448d284ffc5f5510290
Dima Gugava <dimitrigugavinho@gmail.com>
2023-05-09 08:33:48 +0400

    imported data and displayed it

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,21 +1,25 @@
 import "./Content.css";
-import companylogo from "../icons/companylogo.svg";
+import data from "../data.json";
+
 const Content = () => {
+  console.log(data);
   return (
     <div className="content_Container">
-      <div className="content_Box">
-        <img className="companylogo" src={companylogo} />
-        <div className="content_Content_SubBox">
-          <div className="content_Content_Header_Content">
-            <a className="content_Time_Added">5h ago</a>
-            <div className="content_Dot"></div>
-            <a className="content_Job_Type">Full Time</a>
+      {data.map((job) => (
+        <div className="content_Box" key={job.id}>
+          <img className="companylogo" src={job.logo} />
+          <div className="content_Content_SubBox">
+            <div className="content_Content_Header_Content">
+              <a className="content_Time_Added">{job.postedAt}</a>
+              <div className="content_Dot"></div>
+              <a className="content_Job_Type">{job.contract}</a>
+            </div>
+            <a className="content_Position">{job.position}</a>
+            <a className="content_Company_Name">{job.company}</a>
+            <a className="content_country">{job.location}</a>
           </div>
-          <a className="content_Position">Senior Software Engineer</a>
-          <a className="content_Company_Name">Scoot</a>
-          <a className="content_country">United Kingdom</a>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
